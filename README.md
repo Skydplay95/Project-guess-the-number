bf2251ecac8274362faae1f59bac1c19f3008188
Skydplay95 <131533231+Skydplay95@users.noreply.github.com>
2023-12-01 12:25:58 +0000

    add art and readme

---
+++ README.md
@@ -0,0 +1,18 @@
+Day 12 Python Project Courses by Angela Wu : Game Guessing the number 
+
+Today, we didn't have any clue or hint to make this project. I've made it from scratch, i'm pretty happy cause i think i done well and the game works fine.
+
+I didn't face special difficulties, except a little bit to user the return of the function to downgrade number of attempts of the player, bc at first i wanted to code it in brut, but i realize it will be much simpler if the attempts give according to the difficulty choose was in a function.
+
+If you have any advice or comment, fell free to tell me, thank you 
+
+
+#Number Guessing Game Objectives:
+
+# Include an ASCII art logo.
+# Allow the player to submit a guess for a number between 1 and 100.
+# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
+# If they got the answer correct, show the actual answer to the player.
+# Track the number of turns remaining.
+# If they run out of turns, provide feedback to the player.
+# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
--- main.py
+++ main.py
@@ -1,17 +1,8 @@
-#Number Guessing Game Objectives:
-
-# Include an ASCII art logo.
-# Allow the player to submit a guess for a number between 1 and 100.
-# Check user's guess against actual answer. Print "Too high." or "Too low." depending on the user's answer.
-# If they got the answer correct, show the actual answer to the player.
-# Track the number of turns remaining.
-# If they run out of turns, provide feedback to the player.
-# Include two different difficulty levels (e.g., 10 guesses in easy mode, only 5 guesses in hard mode).
-
 #import random
 import random
 
-#from replit import clear
+#import logo
+from art import logo
 
 
 #function guess
@@ -30,7 +21,7 @@ def guess(guess_attempt):
 #function to determine number of attemps
 def user_difficulty(answer):
     """
-    Function take the difficultu as parameter, and return the number of attemps user has
+    Function take the difficulty as parameter, and return the number of attemps user has
     """
 
     if answer == 'easy':
@@ -41,10 +32,17 @@ def user_difficulty(answer):
         return 5
 
 
+print(logo)
+
+print(
+    "Welcome to the guessing game, will you succeed to guess the random number ?"
+)
+print("The number to guess is between 1 and 100.")
+
 #computer choose a random number between 1 and 100
 number_to_guess = random.choice(range(1, 101))
-print(number_to_guess)
 
+#user choose difficultys
 difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
 attemps = user_difficulty(difficulty)
 #print amount of user attemps giving the difficulty choosen
@@ -60,7 +58,7 @@ while not game_end:
 
     #if user guess attemps fall to 1, one more attemps and it's over
     if attemps == 1:
-        print("You ran out of attemps, you loose sorry")
+        print(f"You ran out of attemps,the number was {number_to_guess}, you loose sorry")
         game_end = True
     #if user guess is wrong, minus 1 attempts
     elif guess(user_guess) != number_to_guess:
